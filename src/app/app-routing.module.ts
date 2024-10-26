de884baa0cb56d82bacca63dbca541c1c3bff5fc
garypedru <59663260+faizan3ali@users.noreply.github.com>
2024-10-26 22:09:05 +0500

    Made changes into app-routing-module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', 'title': 'Home', component: HomeComponent},
+    {path:'about', 'title': 'About', component:AboutComponent},
+    {path:'contact', 'title': 'Contact', component:ContactComponent},
+    {path:'**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
