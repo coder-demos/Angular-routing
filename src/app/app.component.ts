5a9c2f66a9da36f14ba3dc4aa1121e6ac48f8bac
garypedru <59663260+faizan3ali@users.noreply.github.com>
2024-10-26 21:29:48 +0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'routing-app';
+    title = 'routing-app';
 }
